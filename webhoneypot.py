4a2212fe6efb264757d4ffb1fb50614706cb8713
Charitha Jayasinghe <153707497+Nuwan-droid@users.noreply.github.com>
2025-12-23 20:33:40 +0530

    Refactor web_honeypot function and fix login logic

--- webhoneypot.py
+++ webhoneypot.py
@@ -29,26 +29,26 @@ def web_honeypot(input_username="admin",input_password="password"):
     @app.route('/wp-admin-login',methods=['POST'])
 
     def login():
-        username = request.form.get['username']
-        password = request.form.get['password']
+        username = request.form.get('username')
+        password = request.form.get('password')
 
         ip_address = request.remote_addr
 
         if username == input_username and password == input_password:
             funnel_logger.info(f"Successful login by {ip_address} with username: {username} and password: {password}")
-            return 'DEEobodaH says Welcome!'
+            return ' Welcome!'
         else:
             funnel_logger.info(f"Failed login attempt by {ip_address} with username: {username} and password: {password}")
             return 'Invalid credintials, Try again.'
         
     return app
 
-def run_web_honeypot(port=5000,input_username="admin",input_password="password"):
+def run_web_honeypot(host="0.0.0.0",port=5000,input_username="admin",input_password="password"):
     run_web_honeypot_app=web_honeypot(input_username,input_password)
     run_web_honeypot_app.run(debug=True,port=port,host="0.0.0.0")
 
     return run_web_honeypot_app
 
 
-run_web_honeypot(port=5000,input_username="admin",input_password="password")
+#run_web_honeypot(port=5000,input_username="admin",input_password="password")
     
